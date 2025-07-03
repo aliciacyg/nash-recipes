b84711429f347f5d1d96da00649bed9a0c2f7138
Alicia Nash <90426191+aliciacyg@users.noreply.github.com>
2025-07-03 11:34:00 -0400

    add script for creating grocery list

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ headers.forEach((header, index) => {
     });
   });
 });
+
+const form = document.getElementById("grocery-form");
+const input = document.getElementById("item-input");
+const list = document.getElementById("grocery-list")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Do not reload the page, that would clear the list
+  const itemText = input.value.trim();
+  if (itemText === "") return; // Don't do anything if no grocery item
+  // Create a checkable list entry
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  // Create the text
+  const span = document.createElement("span");
+  span.textContent = itemText;
+  // Strikethrough when checked (will add later)
+  checkbox.addEventListener("change", () => {
+    span.classList.toggle("checked", checkbox.checked);
+  });
+  
+  list.appendChild(checkbox);
+  list.appendChild(span);
+
+  input.value = "";
+  input.focus();
+})
